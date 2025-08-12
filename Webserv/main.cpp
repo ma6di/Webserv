334237fd641fb008459265f49aea8d7978d9376e
Tayfun Kasapoglu <tayfun.kasapoglu@hotmail.com>
2025-08-12 17:36:57 +0200

    Fixed memory leak and added if statement for multi argv

--- Webserv/main.cpp
+++ Webserv/main.cpp
@@ -20,7 +20,8 @@ volatile sig_atomic_t g_running = 1;
 // Global pointer for cleanup in signal handler
 WebServer* g_server = NULL;
 
-*//**
+*/
+/**
  * SIGINT/SIGTERM handler: Sets running flag to 0 for graceful shutdown
  */
 /*void sigint_handler(int) {
@@ -70,46 +71,63 @@ int main(int argc, char** argv) {
 }*/
 
 volatile bool g_running = true;
-std::vector<WebServer*> g_servers;
+std::vector<WebServer *> g_servers;
 
-static void sigint_handler(int /*signum*/) {
+static void sigint_handler(int /*signum*/)
+{
     g_running = false;
     // Shut down all servers immediately
-    for (size_t i = 0; i < g_servers.size(); ++i) {
+    for (size_t i = 0; i < g_servers.size(); ++i)
+    {
         g_servers[i]->shutdown();
     }
 }
 
-int main(int argc, char** argv) {
-    try {
+int main(int argc, char **argv)
+{
+    try
+    {
         // 1) Determine config file path
         std::string config_file = "default.conf";
-        if (argc > 1) {
+        if (argc > 1)
+        {
             config_file = argv[1];
         }
 
+        if (argc > 1)
+        {
+            std::cout << "Please provide a single config file path as the first argument." << std::endl;
+            std::cout << "If you do not provide any argument, default config file will be used." << std::endl;
+            return 1;
+        }
         // 2) Parse all server blocks
         std::vector<Config> configs = parseConfigFile(config_file);
-        
-        for (size_t ci = 0; ci < configs.size(); ++ci) {
-        const Config& cfg = configs[ci];
-        const std::vector<LocationConfig>& locs = cfg.getLocations();
-        for (size_t li = 0; li < locs.size(); ++li) {
-            const LocationConfig& L = locs[li];
-            // build a comma-separated list of methods
-            std::ostringstream methods;
-            for (size_t mi = 0; mi < L.allowed_methods.size(); ++mi) {
-                if (mi) methods << ",";
-                methods << L.allowed_methods[mi];
+
+        for (size_t ci = 0; ci < configs.size(); ++ci)
+        {
+            const Config &cfg = configs[ci];
+            const std::vector<LocationConfig> &locs = cfg.getLocations();
+            for (size_t li = 0; li < locs.size(); ++li)
+            {
+                const LocationConfig &L = locs[li];
+                // build a comma-separated list of methods
+                std::ostringstream methods;
+                for (size_t mi = 0; mi < L.allowed_methods.size(); ++mi)
+                {
+                    if (mi)
+                        methods << ",";
+                    methods << L.allowed_methods[mi];
+                }
+                Logger::log(LOG_INFO,
+                            "ConfigDump",
+                            "server[" + to_str(ci) + "].loc[" + to_str(li) + "] "
+                                                                             "path='" +
+                                L.path +
+                                "' methods=[" + methods.str() + "] "
+                                                                "upload_dir='" +
+                                L.upload_dir + "'");
             }
-            Logger::log(LOG_INFO,
-                        "ConfigDump",
-                        "server[" + to_str(ci) + "].loc[" + to_str(li) + "] "
-                        "path='" + L.path +
-                        "' methods=[" + methods.str() + "] "
-                        "upload_dir='" + L.upload_dir + "'");
         }
-    }
         // 3) Install signal handlers for graceful shutdown
         struct sigaction sa;
         sa.sa_handler = sigint_handler;
@@ -119,103 +137,134 @@ int main(int argc, char** argv) {
         sigaction(SIGTERM, &sa, 0);
 
         // 4) Instantiate one WebServer per Config
-        for (size_t i = 0; i < configs.size(); ++i) {
-            WebServer* srv = new WebServer(configs[i]);
+        for (size_t i = 0; i < configs.size(); ++i)
+        {
+            WebServer *srv = new WebServer(configs[i]);
             g_servers.push_back(srv);
         }
 
-
         // 5) Unified non-blocking loop
-         while (g_running) {
-        std::vector<struct pollfd> fds;
+        while (g_running)
+        {
+            std::vector<struct pollfd> fds;
 
-        // build the pollfd list
-        for (size_t si = 0; si < g_servers.size(); ++si) {
-            WebServer* srv = g_servers[si];
+            // build the pollfd list
+            for (size_t si = 0; si < g_servers.size(); ++si)
+            {
+                WebServer *srv = g_servers[si];
 
-            // listening sockets (only care about new connections)
-            const std::vector<int>& ls = srv->getListeningSockets();
-            for (size_t j = 0; j < ls.size(); ++j) {
-                struct pollfd pfd;
-                pfd.fd      = ls[j];
-                pfd.events  = POLLIN;
-                pfd.revents = 0;
-                fds.push_back(pfd);
-            }
-
-            // client sockets (watch for read *and* conditional write)
-            std::vector<int> cs = srv->getClientSockets();
-            for (size_t j = 0; j < cs.size(); ++j) {
-                int fd = cs[j];
-                struct pollfd pfd;
-                pfd.fd      = fd;
-                pfd.events  = POLLIN
-                            | (srv->hasPendingWrite(fd) ? POLLOUT : 0);
-                pfd.revents = 0;
-                fds.push_back(pfd);
-            }
-        }
-
-        int ret = poll(&fds[0], fds.size(), -1);
-        if (ret < 0 && errno == EINTR) {
-            break;
-        }
-
-        // handle ready fds
-        for (size_t pi = 0; pi < fds.size(); ++pi) {
-            struct pollfd& p = fds[pi];
-
-            // 1) Incoming connection or data?
-            if (p.revents & POLLIN) {
-                bool handled = false;
-
-                // a) New connection?
-                for (size_t si = 0; si < g_servers.size(); ++si) {
-                    WebServer* srv = g_servers[si];
-                    const std::vector<int>& ls = srv->getListeningSockets();
-                    for (size_t j = 0; j < ls.size(); ++j) {
-                        if (ls[j] == p.fd) {
-                            srv->handleNewConnectionOn(p.fd);
-                            handled = true;
-                            break;
-                        }
-                    }
-                    if (handled) break;
+                // listening sockets (only care about new connections)
+                const std::vector<int> &ls = srv->getListeningSockets();
+                for (size_t j = 0; j < ls.size(); ++j)
+                {
+                    struct pollfd pfd;
+                    pfd.fd = ls[j];
+                    pfd.events = POLLIN;
+                    pfd.revents = 0;
+                    fds.push_back(pfd);
                 }
 
-                // b) Existing client data?
-                if (!handled) {
-                    for (size_t si = 0; si < g_servers.size(); ++si) {
-                        WebServer* srv = g_servers[si];
-                        std::vector<int> cs = srv->getClientSockets();
-                        for (size_t j = 0; j < cs.size(); ++j) {
-                            if (cs[j] == p.fd) {
-                                srv->handleClientDataOn(p.fd);
+                // client sockets (watch for read *and* conditional write)
+                std::vector<int> cs = srv->getClientSockets();
+                for (size_t j = 0; j < cs.size(); ++j)
+                {
+                    int fd = cs[j];
+                    struct pollfd pfd;
+                    pfd.fd = fd;
+                    pfd.events = POLLIN | (srv->hasPendingWrite(fd) ? POLLOUT : 0);
+                    pfd.revents = 0;
+                    fds.push_back(pfd);
+                }
+            }
+
+            int ret = poll(&fds[0], fds.size(), -1);
+
+            if (ret < 0 && errno == EINTR)
+            {
+                break;
+            }
+
+            // handle ready fds
+            for (size_t pi = 0; pi < fds.size(); ++pi)
+            {
+                struct pollfd &p = fds[pi];
+
+                // 1) Incoming connection or data?
+                if (p.revents & POLLIN)
+                {
+                    bool handled = false;
+
+                    // a) New connection?
+                    for (size_t si = 0; si < g_servers.size(); ++si)
+                    {
+                        WebServer *srv = g_servers[si];
+                        const std::vector<int> &ls = srv->getListeningSockets();
+                        for (size_t j = 0; j < ls.size(); ++j)
+                        {
+                            if (ls[j] == p.fd)
+                            {
+                                srv->handleNewConnectionOn(p.fd);
+                                handled = true;
                                 break;
+                            }
+                        }
+                        if (handled)
+                            break;
+                    }
+
+                    // b) Existing client data?
+                    if (!handled)
+                    {
+                        for (size_t si = 0; si < g_servers.size(); ++si)
+                        {
+                            WebServer *srv = g_servers[si];
+                            std::vector<int> cs = srv->getClientSockets();
+                            for (size_t j = 0; j < cs.size(); ++j)
+                            {
+                                if (cs[j] == p.fd)
+                                {
+                                    srv->handleClientDataOn(p.fd);
+                                    break;
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            // 2) Ready to write?
-            if (p.revents & POLLOUT) {
-                // Find which server owns this fd and flush its buffer
-                for (size_t si = 0; si < g_servers.size(); ++si) {
-                    WebServer* srv = g_servers[si];
-                    // If this fd belongs to srv, it will handle it
-                    std::vector<int> cs = srv->getClientSockets();
-                    if (std::find(cs.begin(), cs.end(), p.fd) != cs.end()) {
-                        srv->flushPendingWrites(p.fd);
-                        break;
+                // 2) Ready to write?
+                if (p.revents & POLLOUT)
+                {
+                    // Find which server owns this fd and flush its buffer
+                    for (size_t si = 0; si < g_servers.size(); ++si)
+                    {
+                        WebServer *srv = g_servers[si];
+                        // If this fd belongs to srv, it will handle it
+                        std::vector<int> cs = srv->getClientSockets();
+                        if (std::find(cs.begin(), cs.end(), p.fd) != cs.end())
+                        {
+                            srv->flushPendingWrites(p.fd);
+                            break;
+                        }
                     }
                 }
             }
         }
+
+        for (size_t si = 0; si < g_servers.size(); ++si)
+        {
+            delete g_servers[si];
+        }
+        g_servers.clear();
     }
-    }
-    catch (const std::exception& e) {
+    catch (const std::exception &e)
+    {
         Logger::log(LOG_ERROR, "main", std::string("Fatal error: ") + e.what());
+
+        for (size_t si = 0; si < g_servers.size(); ++si)
+        {
+            delete g_servers[si];
+        }
+        g_servers.clear();
         return 1;
     }
 
